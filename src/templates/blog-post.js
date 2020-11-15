9e5ac6dda04c5245de090711b2d8ed2372c0497f
lfsaraceno <lfsaraceno@hotmail.com>
2020-11-15 08:12:03 -0300

    Add styled no post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,17 +1,31 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout  from "../components/Layout"
+import SEO from "../components/seo"
+import * as S from "../components/PostStyled/styled"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-      <>
-        <h1>Title: {post.frontmatter.title}</h1>
-        <h2>Date: {post.frontmatter.date}</h2>
-        <h3>Category: {post.frontmatter.category}</h3>
-        <p>Time To Read: {post.frontmatter.timeToRead}</p>
-        <div dangerouslySetInnerHTML={{__html: post.html }} ></div>
-      </>
+      <Layout >
+        <SEO title={post.frontmatter.title} />
+        <S.PostStyledHeader>
+
+          <S.PostStyledTitle>{post.frontmatter.title}</S.PostStyledTitle>
+          <S.PostStyledDate> {post.frontmatter.category} • {post.frontmatter.date} • {post.timeToRead} Min. de Leitura</S.PostStyledDate>
+          <S.PostStyledDate></S.PostStyledDate>
+          <S.PostStyledDescription>{post.frontmatter.description}</S.PostStyledDescription>
+
+        </S.PostStyledHeader>
+
+        <S.MainContent><div dangerouslySetInnerHTML={{__html: post.html }} ></div></S.MainContent>
+
+
+
+
+      </Layout>
   )
 
 }
@@ -22,6 +36,7 @@ query blogPost($slug: String) {
       title
       date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       category
+      description
     }
     html
     timeToRead
